814e503abb34024715cbdbcb2de889f4a45a6e6c
torbjkri <torbjorn.kringeland88@gmail.com>
2021-12-04 15:12:40 +0100

    Time functions

--- solutions/solutions.go
+++ solutions/solutions.go
@@ -6,19 +6,60 @@ import (
 	"dec3"
 	"dec4"
 	"fmt"
+	"time"
 )
 
 func main() {
+	start := time.Now()
 	fmt.Println("December 1 - A: ", dec1.Solve1())
-	fmt.Println("December 1 - B: ", dec1.Solve2())
-	fmt.Println()
-	fmt.Println("December 2 - A: ", dec2.Solve1())
-	fmt.Println("December 2 - B: ", dec2.Solve2())
-	fmt.Println()
-	fmt.Println("December 3 - A ", dec3.Solve1())
-	fmt.Println("December 3 - B ", dec3.Solve2())
-	fmt.Println()
-	fmt.Println("December 4 - A ", dec4.Solve1())
-	fmt.Println("December 4 - B ", dec4.Solve2())
+	timeA := time.Since(start).Microseconds()
+	fmt.Println("Dec 1 part A took: ", timeA, "us")
 
+	start = time.Now()
+	fmt.Println("December 1 - B: ", dec1.Solve2())
+	timeB := time.Since(start).Microseconds()
+	fmt.Println("December 1 part B took: ", timeB, "us")
+	fmt.Println("December 1 took in total : ", timeA+timeB, "us")
+	fmt.Println()
+	fmt.Println()
+
+	//// December 2
+	start = time.Now()
+	fmt.Println("December 2 - A: ", dec2.Solve1())
+	timeA = time.Since(start).Microseconds()
+	fmt.Println("Dec 2 part A took: ", timeA, "us")
+
+	start = time.Now()
+	fmt.Println("December 2 - B: ", dec2.Solve2())
+	timeB = time.Since(start).Microseconds()
+	fmt.Println("December 2 part B took: ", timeB, "us")
+	fmt.Println("December 2 took in total : ", timeA+timeB, "us")
+	fmt.Println()
+	fmt.Println()
+	// December 3
+	start = time.Now()
+	fmt.Println("December 3 - A: ", dec3.Solve1())
+	timeA = time.Since(start).Microseconds()
+	fmt.Println("Dec 23 part A took: ", timeA, "us")
+
+	start = time.Now()
+	fmt.Println("December 3 - B: ", dec3.Solve2())
+	timeB = time.Since(start).Microseconds()
+	fmt.Println("December 3 part B took: ", timeB, "us")
+	fmt.Println("December 3 took in total : ", timeA+timeB, "us")
+	fmt.Println()
+	fmt.Println()
+	// December 4
+	start = time.Now()
+	fmt.Println("December 4 - A: ", dec4.Solve1())
+	timeA = time.Since(start).Microseconds()
+	fmt.Println("Dec 4 part A took: ", timeA, "us")
+
+	start = time.Now()
+	fmt.Println("December 4 - B: ", dec4.Solve2())
+	timeB = time.Since(start).Microseconds()
+	fmt.Println("December 4 part B took: ", timeB, "us")
+	fmt.Println("December 4 took in total : ", timeA+timeB, "us")
+	fmt.Println()
+	fmt.Println()
 }
